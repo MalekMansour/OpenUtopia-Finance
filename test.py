24323f609e0e588ce378008edfd8117ca79cc4f9
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2024-09-02 01:38:15 -0600

    Update test.py

--- test.py
+++ test.py
@@ -88,8 +88,8 @@ class OpenUtopiaFinanceApp:
         self.root.bind("<Shift-X>", lambda event: self.edit_income())
         self.root.bind("<Shift-G>", lambda event: self.toggle_grid())
         self.root.bind("<Shift-T>", lambda event: self.change_theme())
-        self.root.bind("<Control-S>", lambda event: self.save_graph())
         self.root.bind("<Shift-Z>", lambda event: self.enable_zoom())
+        self.root.bind("<Control-S>", lambda event: self.save_graph())
         self.root.bind("<Control-Z>", lambda event: self.go_back())
 
     def setup_data_entry_form(self):
