95979e5013f528ccaa016fb8e2c98b6c22775dae
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2024-09-02 01:38:31 -0600

    Update test.py

--- test.py
+++ test.py
@@ -89,8 +89,7 @@ class OpenUtopiaFinanceApp:
         self.root.bind("<Shift-G>", lambda event: self.toggle_grid())
         self.root.bind("<Shift-T>", lambda event: self.change_theme())
         self.root.bind("<Shift-Z>", lambda event: self.enable_zoom())
-        self.root.bind("<Control-S>", lambda event: self.save_graph())
-        self.root.bind("<Control-Z>", lambda event: self.go_back())
+        self.root.bind("<Shift-S>", lambda event: self.save_graph())
 
     def setup_data_entry_form(self):
         """Sets up the data entry form for user input."""
